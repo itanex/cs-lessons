76bdbeda90c6f4e7b76e470575149682d19be18c
Bryan Wood <whenidev@gmail.com>
2017-03-23 13:06:02 -0700

    Add Lesson 01

---
+++ Lesson01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static string[] emails = {
+            "david.jones@proseware.com",
+            "d.j@server1.proseware.com",
+            "jones@ms1.proseware.com",
+            "j.@server1.proseware.com",
+            "j@proseware.com9",
+            "js#internal@proseware.com",
+            "j_9@129.126.118.1",
+            "j..s@proseware.com", // invalid
+            "js*@proseware.com",  // invalid
+            "js@proseware..com",  // invalid
+            "js@proseware.com9",
+            "j.s@server1.proseware.com"
+        };
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            var x = 0;
+            do
+            {
+                if (x % 7 == 0)
+                {
+                    Console.WriteLine(x);
+                }
+                x++;
+            } while (x <= 100);
+
+
+            var a = 0;
+            while (a <= 100)
+            {
+                if (a % 7 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+                a++;
+            }
+
+            // Output a table list using String.Format that displays as such
+            //
+            // Valid   | <valid email>
+            // Invalid | <invalid email>
+
+            // pause
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public int Year { get; set; }
+    }
+}
---
+++ Lesson01/readme.md
@@ -0,0 +1,28 @@
+# Lesson 01
+
+## Topics
+
+### Control Structures
+
+If statements
+Switch statements
+
+### Loops
+
+For Loop - Manual Iterative Loop
+* You have to control the iteration yourself
+* You have control of the exit parameters
+
+Foreach Loop - Automated Iterative Loop
+* Iterates through the the IEnumerable type provided
+* Creates a special scope, so operations may not be as intended
+
+Do While Loop - Post Check Loop
+* Runs at least once
+* Runs indefinately until exit condition is reached
+* Can easily become an infinite loop
+
+While Loop - Pre Check Loop
+* May never run loop if condition is false before loop starts
+* Runs indefinately until exit condition is reached
+* Can easily become an infinite loop
