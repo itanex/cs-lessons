3784c410ff7630e497b5122e500e558bfc36bbe6
Bryan Wood <whenidev@gmail.com>
2017-03-24 13:12:35 -0700

    Add Lesson 13

---
+++ lesson13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string demo1 = null;
+
+            Console.WriteLine("DEMO 1 '{0}'", demo1);
+
+            Product demo2 = null;
+
+            Console.WriteLine("Demo 2 '{0}'", demo2);
+
+            bool? demo3 = null;
+
+            Console.WriteLine("Demo 3 '{0}'", demo3 ?? true);
+        }
+    }
+
+    class Product {
+        public string Name { get; set; }
+    }
+}
---
+++ lesson13/readme.md
@@ -0,0 +1,12 @@
+# Lesson 13
+
+## Topics
+
+### Nullable and Working with Nulls
+
+Example of string 
+
+Example of Custom object
+
+Example of value type being nullable
+
