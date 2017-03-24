6c325f93661fbf07fa8ca6dfceb6a4a916e99b9b
Bryan Wood <whenidev@gmail.com>
2017-03-24 13:14:10 -0700

    Add Lesson 15

---
+++ Lesson15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lesson15
+{
+    class Program
+    {
+        // https://msdn.microsoft.com/library/hh191443(vs.110).aspx
+        
+        public async static Task DoSomething()
+        {
+            var client = new HttpClient();
+            var page = await client.GetStringAsync("http://Microsoft.com");
+
+            /*
+             Wreck the world while waiting!
+             */
+            Console.WriteLine(page);
+        }
+
+        static void Main(string[] args)
+        {
+            // block synchronously until task is completed
+            DoSomething().Wait();
+
+            // pause
+            Console.ReadLine();
+        }
+    }
+}
